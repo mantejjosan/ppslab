f3c7c29908044d469c670f84419648af9e198efc
Mantej <116100820+mantejjosan@users.noreply.github.com>
2024-08-29 10:18:58 +0530

    Add files via upload

---
+++ Mantej CSD/p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string name = "Mantej Singh Josan";
+	int roll = 2415188;
+	string section = "D-1";
+	
+	cout << "Name : " << name << "\n";
+	cout << "Roll Number : " << roll << "\n";
+	cout << "Section : " << section;
+
+	// cout << "Hello Multiverse!" << "\n";
+	return 0;
+}
